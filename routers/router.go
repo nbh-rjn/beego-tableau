ef918ab85f02bc7db3b614dbc7971f4672d1b280
Nabiha <nabiharajani@gmail.com>
2024-07-19 11:05:06 -0700

    major refactor ; attribute endpoint & structs in types.go

---
+++ routers/router.go
@@ -0,0 +1,18 @@
+package routers
+
+import (
+	"beego-project/controllers"
+
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func init() {
+	beego.Router("/", &controllers.TableauController{})
+
+	beego.Router("/authenticate", &controllers.TableauController{}, "post:PostAuth")
+	beego.Router("/sync", &controllers.TableauController{}, "post:PostSync")
+	beego.Router("/download", &controllers.TableauController{}, "get:DownloadDataSource")
+
+	// fetching projects, datasources, datalabels, etc . . .
+	beego.Router("/attribute/:param", &controllers.TableauController{}, "get:GetAttribute")
+}
