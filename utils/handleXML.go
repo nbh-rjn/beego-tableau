e2999140ad46d5c978fdfdbb63eabf64adb163b0
Nabiha <nabiharajani@gmail.com>
2024-07-19 12:42:45 -0700

    minor changes

--- utils/handleXML.go
+++ utils/handleXML.go
@@ -73,3 +73,37 @@ func ExtractAttributes(response *http.Response, attributeType string) ([]map[str
 	return attributes, nil
 
 }
+
+// to extract token from xml response
+func ExtractToken(response *http.Response) (string, error) {
+
+	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+	xmlData := string(responseBody)
+
+	// unmarshal XML using our structs
+	var tsResponse models.AuthResponse
+
+	if err := xml.Unmarshal([]byte(xmlData), &tsResponse); err != nil {
+		return "", err
+	}
+
+	// Extract token from the struct
+	token := tsResponse.Credentials.Token
+	return token, nil
+}
+
+// construct xml request to tableau api
+// using the info from the request to beego
+func CredentialsXML(PersonalAccessTokenName string, PersonalAccessTokenSecret string, ContentUrl string) string {
+	xml := fmt.Sprintf(
+		`<tsRequest>
+		            <credentials personalAccessTokenName="%s"
+		                personalAccessTokenSecret="%s">
+		                	<site contentUrl="%s" />
+		            </credentials>
+		        </tsRequest>`, PersonalAccessTokenName, PersonalAccessTokenSecret, ContentUrl)
+	return xml
+}
