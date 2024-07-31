96bfa68985b37ffdd66b5f0faacc0690a5117be9
Nabiha <nabiharajani@gmail.com>
2024-07-31 01:10:33 -0700

    remove unnecessary site id req field in sync

---
+++ controllers/sync.go
@@ -0,0 +1,133 @@
+package controllers
+
+import (
+	"beego-project/lib"
+	"beego-project/models"
+	"beego-project/utils"
+	"fmt"
+	"net/http"
+)
+
+func (c *TableauController) PostSync() {
+	c.EnableRender = false
+	var errorMsg error = nil
+
+	// parse request to struct
+	var requestBody models.SyncRequest
+	if err := c.BindJSON(&requestBody); err != nil {
+		HandleError(c, http.StatusBadRequest, "Invalid JSON format in request")
+	}
+
+	// get metadata from csv
+	datasourceRecords, err := utils.ParseCSV(requestBody.Filename)
+	if err != nil {
+		HandleError(c, http.StatusInternalServerError, err.Error())
+	}
+
+	// add more tables, cols (assets)
+	if requestBody.CreateNewAssets {
+		for _, datasourceRecord := range datasourceRecords {
+
+			// tds filename
+			fileNameTDS := fmt.Sprintf("%s-%s.tds", datasourceRecord.Datasource, models.Get_siteID())
+			filePath := "storage/" + fileNameTDS
+
+			// get all current data sources
+			currentDatasources, err := lib.TableauGetAttributes("datasources")
+			if err != nil {
+				errorMsg = err
+				continue
+			}
+
+			// check if data source already exists
+			// if it exists we need to recreate to prevent loss when overwriting
+
+			if id := utils.DatasourceExists(currentDatasources, datasourceRecord.Datasource); id != "" {
+
+				// download existing data source
+				filePath, err := lib.TableauDownloadDataSource(id)
+				if err != nil {
+					errorMsg = err
+
+					// we dont continue with the sync if the data source exists but we cant download it
+					// otherwise it will get overwritten
+					continue
+				}
+
+				// extract metadata of existing data source
+				// dvd rentals columns issue
+				datasourceRecord, err = utils.ExtractAssets(filePath, datasourceRecord)
+				if err != nil {
+					errorMsg = err
+				}
+
+			}
+
+			// generate tds file for each datasource struct
+			if err := utils.GenerateTDSFile(filePath, datasourceRecord); err != nil {
+				errorMsg = err
+			}
+
+			// publish it
+			if _, err := lib.TableauPublishDatasource(filePath, fileNameTDS, datasourceRecord.Datasource, requestBody.ProjectID); err != nil {
+				errorMsg = err
+			}
+
+		}
+
+	}
+
+	if errorMsg != nil {
+		HandleError(c, http.StatusInternalServerError, fmt.Sprintf("error creating assets: %s", errorMsg.Error()))
+	}
+
+	// cant label columns in dvdrentals
+	// cols not being recognized in dvdrentals
+
+	errorMsg = nil
+
+	// create label categories
+	lib.TableauCreateCategory(requestBody.AttributeMap.ContentProfile)
+	lib.TableauCreateCategory(requestBody.AttributeMap.DataElements)
+
+	for _, datasourceRecord := range datasourceRecords {
+
+		// loop through all tables
+		for _, table := range datasourceRecord.Tables {
+
+			// get ids of table and its columns
+			tableID, columnIDs, err := lib.TableauGetAssetIDs(datasourceRecord.Database, table.TableName)
+			if err != nil {
+				errorMsg = err
+			}
+
+			// label table
+			if err := lib.TableauLabelAsset(table.ContentProfiles, requestBody.AttributeMap.ContentProfile, "table", tableID); err != nil {
+				errorMsg = err
+			}
+
+			// loop through all columns of table
+			for _, column := range table.Columns {
+
+				// label column
+				columnID := columnIDs[column.ColumnName]
+				if err := lib.TableauLabelAsset(column.DataElements, requestBody.AttributeMap.DataElements, "column", columnID); err != nil {
+					errorMsg = err
+				}
+
+			}
+
+		}
+
+	}
+
+	if errorMsg != nil {
+		HandleError(c, http.StatusInternalServerError, fmt.Sprintf("error labelling assets: %s", errorMsg.Error()))
+	}
+
+	// success message
+	c.Ctx.Output.SetStatus(http.StatusOK)
+	c.Data["json"] = map[string]string{"success": "Records sync-ed successfully"}
+	c.ServeJSON()
+
+}
---
+++ lib/tableauPublishDatasourceRequest.go
@@ -0,0 +1,95 @@
+package lib
+
+import (
+	"beego-project/models"
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
+
+// publishes file
+func TableauPublishDatasource(filePath string, filenameTDS string, datasourceName string, projectID string) (string, error) {
+	url := models.TableauURL() + "sites/" + models.Get_siteID() + "/datasources?datasourceType=tds&overwrite=true"
+
+	// construct request payload
+	requestPayload := fmt.Sprintf(
+		`<tsRequest><datasource name="%s">
+			<project id="%s" />
+		</datasource></tsRequest>`, datasourceName, projectID)
+
+	// buffer to store request
+	requestBody := &bytes.Buffer{}
+
+	// multipart writer
+	writer := multipart.NewWriter(requestBody)
+
+	// add request_payload field
+	if err := writer.WriteField("request_payload", requestPayload); err != nil {
+		return "", err
+	}
+
+	// add tableau_datasource file field
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	// create form file part
+	part, err := writer.CreateFormFile("tableau_datasource", filenameTDS)
+	if err != nil {
+		return "", err
+	}
+
+	// copy file content into the part
+	if _, err := io.Copy(part, file); err != nil {
+		return "", err
+	}
+
+	// close multipart writer, write the boundary
+	if err := writer.Close(); err != nil {
+		return "", err
+	}
+
+	// create HTTP request
+	request, err := http.NewRequest("POST", url, requestBody)
+	if err != nil {
+		return "", err
+	}
+
+	// set headers
+	// do not replace the sprintf with string concatenation
+	request.Header.Set("Content-Type", fmt.Sprintf("multipart/mixed; boundary=%s", writer.Boundary()))
+	request.Header.Set("X-Tableau-Auth", models.Get_token())
+
+	// send HTTP request
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	// Check if the response status code is not OK (200)
+	if response.StatusCode >= 400 {
+		return "", fmt.Errorf("request failed with status code: %d", response.StatusCode)
+	}
+
+	var extractresponse models.PublishDSResponse
+
+	// Unmarshal the XML data into the struct
+	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if err := xml.Unmarshal(responseBody, &extractresponse); err != nil {
+		return "", err
+	}
+
+	return extractresponse.Datasource.Id, nil
+}
